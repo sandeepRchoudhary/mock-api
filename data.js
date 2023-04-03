0484479c88b8f8a02fbc652bc2be4150801d56ef
sandeep <reychoudhary07@gmail.com>
2023-04-03 22:50:38 +0530

    api implementation

---
+++ data.js
@@ -0,0 +1,5 @@
+export const dynamicData=[
+    {_id:1,name:"sandeep",email:"sandeep@gmail.com",phone:"7700044747"},
+    {_id:2,name:"sanish",email:"sanish@gmail.com",phone:"8800044747"},
+    {_id:3,name:"sandhya",email:"sandhya@gmail.com",phone:"9900044747"}
+]
